e7601e3e0b97ed5df47e703f850a4812360293f9
summernine <enujare@gmail.com>
2020-09-25 11:16:24 +1000

    Update aupost-api-domestic.py

--- api-test/exercises/aupost-api-domestic.py
+++ api-test/exercises/aupost-api-domestic.py
@@ -22,7 +22,7 @@ service_code = [
 def test_get_postage_domestic_services_status_code_within_AU(fr, to):
     domestic_url = "https://digitalapi.auspost.com.au/postage/parcel/domestic"
     auth = {"AUTH-KEY": "60c2ab35-bff5-4082-9184-a6f8ec713a81"}
-    response = requests.get(f"{domestic_url}/service.json?from_postcode={fr}&to_postcode={to}&lenth=22&width=16&length=7.7&height=12&weight=1.5",
+    response = requests.get(f"{domestic_url}/service.json?from_postcode={fr}&to_postcode={to}&length=22&width=16&height=12&weight=1.5",
                             headers = auth)
     print(response.json()["services"]["service"][1])
 
@@ -35,7 +35,7 @@ def test_get_postage_domestic_services_status_code_within_AU(fr, to):
 def test_get_postage_domestic_cost_within_AU(fr, to, service_code, service_name):
     domestic_url = "https://digitalapi.auspost.com.au/postage/parcel/domestic"
     auth = {"AUTH-KEY": "60c2ab35-bff5-4082-9184-a6f8ec713a81"}
-    response = requests.get(f"{domestic_url}/calculate.json?from_postcode={fr}&to_postcode={to}&lenth=22&width=16&length=7.7&height=12&weight=1.5&service_code=" + service_code,
+    response = requests.get(f"{domestic_url}/calculate.json?from_postcode={fr}&to_postcode={to}&length=22&width=16&height=12&weight=1.5&service_code={service_code}",
                             headers= auth)
     print(response.json())
 
